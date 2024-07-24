b5d3a096f8c56a375452f96a15ebc9b8e3270a29
Ari Gonzalez <arigonzalez515@gmail.com>
2024-07-24 19:29:03 -0400

    add vite

--- app/head.tsx
+++ app/head.tsx
@@ -30,14 +30,14 @@ export default function Head() {
       <meta property='og:title' content={title} />
       <meta property='og:type' content='site' />
       <meta property='og:url' content={url} />
-      <meta property='og:image' content={'/icons/share.png'} />
+      <meta property='og:image' content={'/favicon/favicon-16x16.png'} />
       <meta property='og:site_name' content={title} />
       <meta property='og:description' content={description} />
 
       <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
-      <link rel='apple-touch-icon' sizes='16x16' href='/icons/favicon-16x16.png' />
-      <link rel='apple-touch-icon' sizes='32x32' href='/icons/favicon-32x32.png' />
-      <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+      <link rel='apple-touch-icon' sizes='16x16' href='/favicon/favicon-16x16.png' />
+      <link rel='apple-touch-icon' sizes='32x32' href='/favicon/favicon-32x32.png' />
+      <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
       <link rel='manifest' href='/manifest.json' />
       <link rel='mask-icon' color='#000000' href='/icons/safari-pinned-tab.svg' />
       <link rel='apple-touch-startup-image' href='/startup.png' />
@@ -47,7 +47,7 @@ export default function Head() {
         <meta name="HandheldFriendly" content="true"/>  */}
       <meta name='viewport' content='width=device-width, minimum-scale=1, initial-scale=1.0' />
       <meta name='theme-color' content='#000' />
-      <link rel='shortcut icon' href='/icons/apple-touch-icon.png' />
+      <link rel='shortcut icon' href='/favicon/apple-touch-icon.png' />
 
       {/* 
       Twitter Summary card
